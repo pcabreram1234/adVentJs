782402a3ead3e849a167e5b4c2ce7ae6bde1e29c
Phillip Leonardo Cabrera Medrano <PCabreraM@edenorte.com.do>
2022-01-26 15:08:09 -0400

    Trabajando con el rto 2

--- listGifts.js
+++ listGifts.js
@@ -1,26 +1,35 @@
 function listGifts(letter) {
   // ¡Tú puedes!
-  let gifts = letter.split(" ");
-  let list = [];
-  let finalList = [];
-  gifts.filter((gift) => {
-    if (gift.indexOf("_") !== 0) {
-      finalList.push(gift);
-    }
-  });
+  /*   let gifts = letter.split(" "); */
 
-  finalList.sort();
+  let expReg = new RegExp(/\s/gi);
+  let gifts = letter.replace(expReg, " ");
+  let giftsObeject = gifts.split(" ")
+  let listWithOut_ = []
+  let finalList = []
+  gifts.split(" ")
 
-  for (let i = 0; i < finalList.length; i++) {
-    if (finalList[i] === finalList[i++]) {
-      list.push(finalList[i]);
-    } else {
-      list.push(finalList[i]);
+  for (let i = 0; i < giftsObeject.length; i++) {
+    if (giftsObeject[i].includes("_") === false) {
+      listWithOut_.push(giftsObeject[i])
     }
   }
 
-  return list;
+  for (const i of listWithOut_) {
+    let count = 0
+    let actualItem = i
+    for (let e = 0; e < listWithOut_.length; e++) {
+      if (listWithOut_[e] === i) {
+        count++
+        finalList.push(i, count)
+      } if (listWithOut_.indexOf(i) === listWithOut_.length - 1) {
+
+      }
+    }
+  }
+
+  return finalList;
 }
-const carta = "bici coche balón _playstation bici coche peluche";
+const carta = "bici coche balón _playstation bici scoche peluche";
 
 console.log(listGifts(carta));
